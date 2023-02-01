abd85dce9c7bffe349bb12f740d0db9b060d80a5
JohnnieV <johnnie.v1986@gmail.com>
2023-02-01 17:25:15 +0400

    finish project

---
+++ Destiny/Controller/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Destiny
+//
+//  Created by Владимир Смирнов on 01.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var storyBrain = StoryBrain()
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        
+    }
+
+    
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle!
+        storyBrain.nextStory(userChoice)
+        updateUI()
+    }
+    
+    func updateUI() {
+        
+        storyLabel.text = storyBrain.getStoryText()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+}
+
